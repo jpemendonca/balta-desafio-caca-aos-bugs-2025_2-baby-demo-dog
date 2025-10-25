a65df70a4f25e4b577e67eb08a158a625f6dc967
Pedro <jpemendonca@outlook.com>
2025-10-25 13:23:12 -0300

    better map on endpoints

--- src/Endpoints/Customer/CustomerEndpoints.cs
+++ src/Endpoints/Customer/CustomerEndpoints.cs
@@ -7,29 +7,32 @@ public static class CustomerEndpoints
 {
     public static void MapCustomerEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/v1/customers", async (
-                [FromServices] ICustomerHandler handler) =>
+        var group = app.MapGroup("/v1/customers");
+        group.WithTags("Customers");
+        
+        group.MapGet("/", async (
+                [FromServices] ICustomerHandler handler) => 
             await handler.GetAllAsync());
 
-        app.MapGet("/v1/customers/{id}", async (
-                [FromRoute] Guid id,
-                [FromServices] ICustomerHandler handler) =>
+        group.MapGet("/{id}", async (
+                [FromRoute] Guid id, 
+                [FromServices] ICustomerHandler handler) => 
             await handler.GetByIdAsync(id));
-
-        app.MapPost("/v1/customers", async (
-                [FromBody] BugStore.Requests.Customers.Create request,
-                [FromServices] ICustomerHandler handler) =>
+        
+        group.MapPost("/", async (
+                [FromBody] BugStore.Requests.Customers.Create request, 
+                [FromServices] ICustomerHandler handler) => 
             await handler.CreateAsync(request));
-
-        app.MapPut("/v1/customers/{id}", async (
-                [FromRoute] Guid id,
-                [FromBody] BugStore.Requests.Customers.Update request,
-                [FromServices] ICustomerHandler handler) =>
+        
+        group.MapPut("/{id}", async (
+                [FromRoute] Guid id, 
+                [FromBody] BugStore.Requests.Customers.Update request, 
+                [FromServices] ICustomerHandler handler) => 
             await handler.UpdateAsync(id, request));
-
-        app.MapDelete("/v1/customers/{id}", async (
-                [FromRoute] Guid id,
-                [FromServices] ICustomerHandler handler) =>
+        
+        group.MapDelete("/{id}", async (
+                [FromRoute] Guid id, 
+                [FromServices] ICustomerHandler handler) => 
             await handler.DeleteAsync(id));
     }
 }
